a500311a1f29be114ce50d040c0f88808c441ece
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-08 18:11:44 +0000

    corregir

--- src/models.py
+++ src/models.py
@@ -1,54 +1,62 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime
+import datetime
+from sqlalchemy import Text
 from typing import List
+from sqlalchemy import Table
+from sqlalchemy import Column
 
 db = SQLAlchemy()
 
+# Tabla de asociación entre usuarios y planetas favoritos
+user_planet_favorites = Table(
+    "user_planet_favorites",
+    db.Model.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("planet_id", ForeignKey("planets.id"), primary_key=True)
+)
+# Tabla de asociación entre usuarios y personas favoritas
+user_people_favorites = Table(
+    "user_people_favorites",
+    db.Model.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("character_id", ForeignKey("people.id"), primary_key=True)
+)
+
+
 class User(db.Model):
+    __tablename__ = "users"  
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     user_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    firts_ame: Mapped[str] = mapped_column(String(120), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120),nullable=False)
     last_name: Mapped[str] = mapped_column(String(120), nullable=False)
-    subscription_date: Mapped[int] = mapped_column(nullable=False)
-    save_data: Mapped[str]= mapped_column(String(5000))
+    subscription_date: Mapped[datetime.datetime] = mapped_column(DateTime)
 
-    #favorite relationship
-    favorites: Mapped[List["Favorite"]] = relationship(back_populates="user")
-    
+
+    # Favorite Relationship
+    favorite_planets: Mapped[List["Planets"]] = relationship(
+        secondary=user_planet_favorites, backref="favorited_by_users"
+    )
+
+    favorite_people: Mapped[List["People"]] = relationship(
+        secondary=user_people_favorites, backref="favorited_by_users"
+    )
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
 
 class Planets(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name_of_planets: Mapped[str] = mapped_column(String(120), nullable=False)
 
-    #favorite relationship
-    favorite_id: Mapped[int] = mapped_column(ForeignKey("favorite.id"))
-    favorite: Mapped["Favorite"] = relationship(back_populates="planets")
- 
 class People(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-
-    #favorite relationship
-    favorite_id: Mapped[int] = mapped_column(ForeignKey("favorite.id"))
-    favorite: Mapped["Favorite"] = relationship(back_populates="people")
-
-class Favorite(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    #User relationship
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="favorites")
-
-    #Planets relationship
-    planets: Mapped[List["Planets"]] = relationship(back_populates="favorite")
-
-    #People relationship
-    people: Mapped[List["People"]] = relationship(back_populates="favorite")
+    name_of_people: Mapped[str] = mapped_column(String(120), nullable=False)
